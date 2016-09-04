70d4f401b300fa897c424222ec7f6cadb9231f8d
Mykhailo Danylov <mike.danylov@gmail.com>
2016-09-04 23:28:33 +0300

    Implement text highlight based on query parameter

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule }               from '@angular/core';
-import { BrowserModule }          from '@angular/platform-browser';
-import { FormsModule }            from '@angular/forms';
-import { HttpModule }             from '@angular/http';
-import                                 './rxjs-extensions';
+import { NgModule }                 from '@angular/core';
+import { BrowserModule }            from '@angular/platform-browser';
+import { FormsModule }              from '@angular/forms';
+import { HttpModule }               from '@angular/http';
+import                                  './rxjs-extensions';
 
-import { AppComponent }           from './app.component';
-import { LoginComponent }         from './login/login.component';
-import { DocumentsListComponent } from './documents-list/documents-list.component';
-import { DocumentComponent }      from './document/document.component';
-import { LoginService }           from './services/login.service';
-import { DocumentService }        from './services/document.service';
-import { routing }                from './app.routing';
+import { AppComponent }             from './app.component';
+import { LoginComponent }           from './login/login.component';
+import { DocumentsListComponent }   from './documents-list/documents-list.component';
+import { DocumentComponent }        from './document/document.component';
+import { LoginService }             from './services/login.service';
+import { DocumentService }          from './services/document.service';
+import { routing }                  from './app.routing';
 
 @NgModule({
   imports: [
--- app/document/document.component.ts
+++ app/document/document.component.ts
@@ -16,6 +16,8 @@ declare var jwt_decode: any;
 
 export class DocumentComponent implements OnInit {
     private document: Document;
+    private searchParam: string;
+    private contentElemSel = 'pre.doc-content';
 
     constructor(
         private router: Router,
@@ -24,7 +26,7 @@ export class DocumentComponent implements OnInit {
         private documentService: DocumentService) {
         }
     
-    private getDocument(docId: number) {
+    private getDocument(docId: number): Promise<Document> {
         let token;
         let tokenPromise;
         let self = this;
@@ -38,7 +40,7 @@ export class DocumentComponent implements OnInit {
             tokenPromise = Promise.resolve();
         }
 
-        tokenPromise.then(() => {
+        return tokenPromise.then(() => {
             token = this.loginService.getLocalToken();
 
             Promise.all([
@@ -46,18 +48,27 @@ export class DocumentComponent implements OnInit {
                 this.documentService.getDocumentContent(token, docId)
             ])
             .then((vals) => {
-                self.appendDocumentContent(vals);
+                return self.appendDocumentContent(vals);
             });
         });
     }
 
-    appendDocumentContent(vals: any) {
+    private parseDocName(doc: Document): Document {
+        doc.name = doc.name.replace(/_|-/g, ' ');
+        return doc;
+    }
+
+    private appendDocumentContent(vals: any): Promise<Document> {
         this.document = vals[0];
+        this.document = this.parseDocName(this.document);
         this.document.content = vals[1];
+        
+        return Promise.resolve(this.document);
     }
 
     ngOnInit(): void {
         let docId: number;
+        let self = this;
 
         if (!this.loginService.isToken()) {
             this.gotoLogin();
@@ -70,15 +81,60 @@ export class DocumentComponent implements OnInit {
                 return;
             }
 
-            this.getDocument(docId);
+            this.getDocument(docId)
+                .then((doc) => {
+                    setTimeout(() => {
+                        self.searchParam = self.getQueryParam('search', location.href);
+                        
+                        if (!self.searchParam) {
+                            return;
+                        }
+                        
+                        self.highlight();
+                    }, 5000);
+                });
         });
+    }
+
+    ngOnChanges(changes: any) {
+        this.searchParam = this.getQueryParam('search', location.href);
+                    
+        if (!this.searchParam) {
+            return;
+        }
+
+        this.highlight();
     }
 
     gotoLogin(): void {
         this.router.navigate(['/login']);
     }
 
-    onClick(): void {
-        console.log(this.document);
+    private highlight() {
+        let content = document.querySelector(this.contentElemSel);
+        let re = new RegExp(this.searchParam, 'g');
+
+        content.innerHTML =
+            content.innerHTML.replace(re, `<span class="highlight">${this.searchParam}</span>`);
+    }
+
+    private getQueryParam(name: string, url: string): string {
+        let regex: RegExp;
+        let results: Array<string>;
+
+        if (!url) {
+            url = window.location.href;
+        }
+
+        name = name.replace(/[\[\]]/g, "\\$&");
+        
+        regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+        results = regex.exec(url);
+        
+        if (!results || !results[2]) {
+            return;
+        }
+        
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
     }
 }
